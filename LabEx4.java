6e94af4ede359b89b0fbdf196d7dc8c3bd4fa33c
Neo Balignasay <131667279+kidisakid@users.noreply.github.com>
2023-04-25 10:41:34 +0800

    Add files via upload

---
+++ LabEx4.java
@@ -0,0 +1,13 @@
+/*Balignasay, Neo Genesis
+Viray, Josh Kenn
+ICS2605
+1CSF
+Lab Exercise 4 */
+
+public class LabEx4 
+{
+    public static void main(String[] args) throws Exception 
+    {
+
+    }
+}
